b17c49c97629477f505da9ef30cd36b9ac50fb37
EngOmondi <fidelotieno167@gmail.com>
2022-09-26 11:30:05 +0300

    add all files

---
+++ firstproject/firstapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Create a path object defining the URL pattern to the index view
+    path(route='', view=views.index, name='index'),
+]
+urlpatterns = [
+    # Create a path object defining the URL pattern to the index view
+    path(route='', view=views.index, name='index'),
+    # Add another path object defining the URL pattern using `/date` prefix
+    path(route='date', view=views.get_date, name='date'),
+]
